d7e8d18a2f3c1db61422c3b6a04f453a309cf23a
fetrojan <felipetrojan@hotmail.com>
2024-02-07 17:14:14 -0300

    Criação da página ViagemExplora

---
+++ js/script.js
@@ -0,0 +1,297 @@
+var imagens = [
+  {
+    src: "./img/athens.png",
+    titulo: "Atenas",
+    subtitulo: "O Berço da Civilização Ocidental",
+    texto:
+      "Atenas, a cidade que testemunhou os alicerces da democracia e da filosofia, é um fascinante encontro entre o antigo e o moderno. A Acrópole domina o horizonte, enquanto as ruas de Plaka contam histórias de séculos passados. Uma jornada única através da história e cultura grega.",
+    subimages: [
+      {
+        src: "./img/acropole.png",
+        titulo: "Acrópole de Atenas",
+        subtitulo: "Coração Histórico da Cidade",
+        texto:
+          "Explore o coração histórico de Atenas subindo até a Acrópole, onde o Partenon, o Erecteion e outros monumentos antigos oferecem uma vista deslumbrante da cidade. Descubra a rica história e a arquitetura espetacular desses locais emblemáticos.",
+      },
+      {
+        src: "./img/agoraAntiga.png",
+        titulo: "Ágora Antiga",
+        subtitulo: "Centro Comercial e Social Antigo",
+        texto:
+          "Mergulhe na Ágora Antiga, o antigo centro comercial e social de Atenas. Caminhe pelas ruínas e imagine a vida cotidiana na Grécia Antiga enquanto explora templos, estátuas e estruturas históricas.",
+      },
+      {
+        src: "./img/plaka.png",
+        titulo: "Plaka - Bairro Tradicional",
+        subtitulo: "Encanto nas Ruas de Paralelepípedos",
+        texto:
+          "Passeie pelo encantador bairro de Plaka, conhecido por suas ruas de paralelepípedos, arquitetura tradicional e atmosfera acolhedora. Descubra lojas locais, tabernas autênticas e uma variedade de arte e artesanato.",
+      },
+      {
+        src: "./img/museu-acropole.png",
+        titulo: "Museu da Acrópole",
+        subtitulo: "Explorando a História Grega",
+        texto:
+          "Visite o Museu da Acrópole, que abriga uma impressionante coleção de artefatos arqueológicos relacionados à Acrópole de Atenas. A exposição oferece uma visão fascinante da história grega, desde os tempos antigos até a era romana.",
+      },
+    ],
+  },
+  {
+    src: "./img/mykonos.png",
+    titulo: "Mikonos",
+    subtitulo: "A Ilha das Festas e das Águas Turquesa",
+    texto:
+      "Mikonos, a joia das Cíclades, é famosa por suas praias deslumbrantes, vida noturna animada e arquitetura encantadora. Com moinhos de vento icônicos e casas brancas reluzentes, Mikonos oferece uma experiência única para quem busca sol, mar e diversão.",
+    subimages: [
+      {
+        src: "./img/nightLife.png",
+        titulo: "Vida Noturna Animada",
+        subtitulo: "Noite em Mikonos",
+        texto:
+          "Mikonos é famosa por sua vida noturna vibrante. Explore clubes animados e eventos culturais sob as estrelas.",
+      },
+      {
+        src: "./img/moinhos.png",
+        titulo: "Moinhos de Vento Pitorescos",
+        subtitulo: "Ícones de Mikonos",
+        texto:
+          "Os icônicos moinhos de vento de Mikonos oferecem uma vista pitoresca da ilha. Capture momentos inesquecíveis sob o sol.",
+      },
+      {
+        src: "./img/whiteHouses.png",
+        titulo: "Casas Brancas Reluzentes",
+        subtitulo: "Arquitetura Encantadora",
+        texto:
+          "Passeie pelas ruas estreitas de Mikonos, onde casas brancas reluzentes criam uma atmosfera encantadora.",
+      },
+      {
+        src: "./img/beaches.png",
+        titulo: "Praias Deslumbrantes",
+        subtitulo: "Areias Douradas",
+        texto:
+          "Relaxe nas praias deslumbrantes de Mikonos, onde areias douradas encontram águas cristalinas. Um refúgio paradisíaco.",
+      },
+    ],
+  },
+  {
+    src: "./img/santorini.png",
+    titulo: "Santorini",
+    subtitulo: "Pôr do Sol Inesquecível e Paisagens de Cartão Postal",
+    texto:
+      "Santorini, com suas icônicas casas brancas e cúpulas azuis, é um paraíso para os amantes de paisagens deslumbrantes. As praias de areia vulcânica, as vinícolas encantadoras e o inesquecível pôr do sol em Oia fazem desta ilha um destino verdadeiramente romântico.",
+    subimages: [
+      {
+        src: "./img/oia.png",
+        titulo: "Pôr do Sol em Oia",
+        subtitulo: "Espetáculo Noturno",
+        texto:
+          "Não perca o espetáculo de cores quando o sol se põe em Oia, criando uma paisagem celestial.",
+      },
+      {
+        src: "./img/vinicolas.png",
+        titulo: "Vinícolas Encantadoras",
+        subtitulo: "Degustação de Vinhos",
+        texto:
+          "Explore as vinícolas de Santorini e prove vinhos locais, enriquecendo sua experiência culinária na ilha.",
+      },
+      {
+        src: "./img/volcanic.png",
+        titulo: "Praias de Areia Vulcânica",
+        subtitulo: "Mar e Areia Exclusivos",
+        texto:
+          "Relaxe nas praias de areia vulcânica de Santorini, oferecendo uma experiência única em meio às águas cristalinas.",
+      },
+      {
+        src: "./img/village.png",
+        titulo: "Aldeias Tradicionais",
+        subtitulo: "Encanto nas Colinas",
+        texto:
+          "Explore as aldeias tradicionais de Santorini, aninhadas nas colinas, onde a arquitetura única e as vistas panorâmicas cativam os visitantes.",
+      },
+    ],
+  },
+  {
+    src: "./img/rhodes.png",
+    titulo: "Rhodes",
+    subtitulo: "História Medieval e Paisagens Exuberantes",
+    texto:
+      "Rhodes, com seu centro histórico medieval e o impressionante Palácio dos Cavaleiros, transporta os visitantes de volta à Idade Média. Além da rica herança histórica, a ilha oferece praias deslumbrantes e uma atmosfera encantadora.",
+    subimages: [
+      {
+        src: "./img/palace.png",
+        titulo: "Palácio dos Cavaleiros",
+        subtitulo: "Tesouro Histórico",
+        texto:
+          "Explore o impressionante Palácio dos Cavaleiros em Rhodes, um tesouro histórico que transporta os visitantes à Idade Média.",
+      },
+      {
+        src: "./img/oldtown.png",
+        titulo: "Centro Histórico Medieval",
+        subtitulo: "Ruas de Pedra",
+        texto:
+          "Passeie pelo centro histórico medieval de Rhodes, onde ruas de pedra e arquitetura preservada contam a história da ilha.",
+      },
+      {
+        src: "./img/beaches-rho.png",
+        titulo: "Praias Deslumbrantes",
+        subtitulo: "Areias Douradas",
+        texto:
+          "Desfrute de praias deslumbrantes em Rhodes, com areias douradas e águas cristalinas, ideais para um dia relaxante à beira-mar.",
+      },
+      {
+        src: "./img/ruins.png",
+        titulo: "Ruínas Antigas",
+        subtitulo: "Vestígios do Passado",
+        texto:
+          "Descubra as ruínas antigas de Rhodes, testemunhando a rica herança histórica da ilha, desde templos até anfiteatros bem preservados.",
+      },
+    ],
+  },
+  {
+    src: "./img/creta.png",
+    titulo: "Creta",
+    subtitulo: "Mitos Antigos e Belezas Naturais",
+    texto:
+      "Creta, a maior das ilhas gregas, é rica em mitologia e beleza natural. De Knossos, o lendário palácio minoico, às paisagens montanhosas e praias idílicas, Creta é um convite para explorar sua rica herança cultural e suas maravilhas naturais.",
+    subimages: [
+      {
+        src: "./img/knossos.png",
+        titulo: "Palácio Minoico de Knossos",
+        subtitulo: "Mitos Antigos Revelados",
+        texto:
+          "Explore o lendário Palácio Minoico de Knossos, onde mitos antigos são revelados através de artefatos e arquitetura.",
+      },
+      {
+        src: "./img/montains.png",
+        titulo: "Paisagens Montanhosas",
+        subtitulo: "Cenas Majestosas",
+        texto:
+          "Creta oferece paisagens montanhosas espetaculares, proporcionando uma visão incrível da natureza e uma oportunidade para os amantes do trekking.",
+      },
+      {
+        src: "./img/idyllic.png",
+        titulo: "Praias Idílicas",
+        subtitulo: "Retiro à Beira-Mar",
+        texto:
+          "Relaxe em praias idílicas de Creta, onde o mar Egeu encontra a beleza natural da ilha, proporcionando um refúgio sereno.",
+      },
+      {
+        src: "./img/villageCrete.png",
+        titulo: "Aldeias Tradicionais",
+        subtitulo: "Cultura e Autenticidade",
+        texto:
+          "Explore as aldeias tradicionais de Creta, onde a cultura local e a autenticidade se manifestam em cada esquina, oferecendo uma experiência única.",
+      },
+    ],
+  },
+  {
+    src: "./img/zakynthos.png",
+    titulo: "Zakynthos",
+    subtitulo: "Paraísos de Praias e a Magia da Gruta Azul",
+    texto:
+      "Zakynthos, conhecida por suas praias paradisíacas e a Gruta Azul, é um destino cativante. As águas cristalinas, as falésias dramáticas e as pitorescas vilas fazem desta ilha um refúgio sereno para os amantes da natureza e da beleza costeira.",
+    subimages: [
+      {
+        src: "./img/villages-zak.png",
+        titulo: "Vilas Pitorescas",
+        subtitulo: "Atmosfera Serena",
+        texto:
+          "Explore vilas pitorescas de Zakynthos, onde o charme e a serenidade se encontram à beira-mar, criando uma atmosfera relaxante e acolhedora.",
+      },
+      {
+        src: "./img/bluecaves.png",
+        titulo: "Gruta Azul",
+        subtitulo: "Magia Subaquática",
+        texto:
+          "Zakynthos é conhecida pela Gruta Azul, um local mágico onde as águas cristalinas revelam um espetáculo subaquático único e encantador.",
+      },
+      {
+        src: "./img/cliffs.png",
+        titulo: "Falésias Dramáticas",
+        subtitulo: "Beleza nas Alturas",
+        texto:
+          "Desfrute das dramáticas falésias de Zakynthos, oferecendo visões panorâmicas das águas azuis abaixo e uma atmosfera de tirar o fôlego.",
+      },
+      {
+        src: "./img/beaches-zak.png",
+        titulo: "Praias Paradisíacas",
+        subtitulo: "Cristalinas e Serenas",
+        texto:
+          "Zakynthos é um destino cativante com praias paradisíacas de águas cristalinas e areias douradas, proporcionando um refúgio sereno para os amantes da natureza.",
+      },
+    ],
+  },
+];
+
+function changeContent(index) {
+    // Atualiza o conteudo da seção "carrossel"
+    updateCarrosselContent(index);
+  
+    // Atualiza o conteudo da seção "atracoes"
+    updateAtracoesContent(index);
+  }
+  
+  function updateCarrosselContent(index) {
+    var mainImage = document.getElementById("imagebox");
+    var mainText = document.getElementById("mainText");
+    var mainTitle = document.getElementById("mainTitle");
+    var mainSubtitle = document.getElementById("mainSubtitle");
+  
+    mainImage.src = imagens[index].src;
+    mainText.innerHTML = imagens[index].texto;
+    mainTitle.innerHTML = imagens[index].titulo;
+    mainSubtitle.innerHTML = imagens[index].subtitulo;
+  }
+  
+  function updateAtracoesContent(index) {
+    var atracoesImagesPequenas = document.querySelectorAll(".atracoes__imagens-pequenas img");
+    var cardMainImage = document.getElementById("card-mainImage");
+    var cardMainTitle = document.getElementById("card-mainTitle");
+    var cardMainSubtitle = document.getElementById("card-mainSubtitle");
+    var cardMainText = document.getElementById("card-mainText");
+  
+    // Atualiza as imagens pequenas
+    atracoesImagesPequenas.forEach(function (element, i) {
+      element.src = imagens[index].subimages[i].src;
+  
+      // Evento para mudar o conteudo de acordo com as imagens pequenas
+      element.onclick = function () {
+        updateAtracoesLargeImageContent(index, i);
+      };
+    });
+  
+    // Atualiza a imagem grande e o conteudo
+    updateAtracoesLargeImageContent(index, 0);
+  }
+  
+  function updateAtracoesLargeImageContent(mainIndex, subIndex) {
+    var cardMainImage = document.getElementById("card-mainImage");
+    var cardMainTitle = document.getElementById("card-mainTitle");
+    var cardMainSubtitle = document.getElementById("card-mainSubtitle");
+    var cardMainText = document.getElementById("card-mainText");
+  
+    // Utiliza o indices principais e sub para atualizar o conteudo
+    cardMainImage.src = imagens[mainIndex].subimages[subIndex].src;
+    cardMainTitle.innerHTML = imagens[mainIndex].subimages[subIndex].titulo;
+    cardMainSubtitle.innerHTML = imagens[mainIndex].subimages[subIndex].subtitulo;
+    cardMainText.innerHTML = imagens[mainIndex].subimages[subIndex].texto;
+  }
+  
+  changeContent(0);
+
+
+  // Swiper
+
+  const swiper = new Swiper(".swiper", {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
